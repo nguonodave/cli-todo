75dc0b09ade9f8e61c1d56f1def23874e9e29864
David Ochiel <nguonodave@gmail.com>
2024-11-24 16:32:45 +0300

    feat: add string literal for terminal options

---
+++ go.mod
@@ -0,0 +1,3 @@
+module clitodo
+
+go 1.22.2
---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(
+`Select one of the following options:
+
+* 1 to Add Task.
+* 2 to View Tasks.
+* 3 to Delete Task.
+* 4 to Update Task.
+* 5 to Exit the program.`,
+	)
+}
